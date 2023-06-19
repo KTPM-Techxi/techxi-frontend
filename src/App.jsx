a5fde9668391fbccf1b03b7a4f6b36ea50d8852d
nhatminhdev <heomeco2014@gmail.com>
2023-06-19 22:19:55 +0700

    init: init front end

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+	return <div className="h-full">
+		<h1 className='max-w-full w-max mx-auto bg-blue-500 rounded-3xl p-4 text-3xl my-auto'>Hello babe</h1>
+	</div>
+}
+
+export default App;
